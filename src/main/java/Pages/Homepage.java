456d3b094ecae175ec3d6391219c6ff77a79ffb9
Priyanka <Priya.patil6614@gmail.com>
2025-07-03 15:58:32 +0530

    Added Waits in project

--- src/main/java/Pages/Homepage.java
+++ src/main/java/Pages/Homepage.java
@@ -1,79 +1,96 @@
 package Pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Homepage {
 
 	WebDriver driver;
-	
-	
-	@FindBy(xpath="//span[text()='Shopping Cart']") private WebElement shoppingcart;
-	@FindBy(xpath="//a[text()='iPhone']") private WebElement iphone;
-	@FindBy(xpath="//a[text()='Apple']") private WebElement apple;
-	@FindBy(xpath="//span[text()='My Account']") private WebElement Myacc;
-	@FindBy(xpath="(//a[text()='Login'])[1]") private WebElement loginopt;
-	@FindBy(xpath="//input[@id=\"input-email\"]") private WebElement Email;
-	@FindBy(xpath="//input[@id=\"input-password\"]") private WebElement pass;
-	@FindBy(xpath="//input[@class=\"btn btn-primary\"]") private WebElement loginbtn;
-	@FindBy(xpath="//h2[text()='My Account']") private WebElement myaccpage;
-	
-	
+	WebDriverWait wait;
+
+	@FindBy(xpath = "//span[text()='Shopping Cart']")
+	private WebElement shoppingcart;
+	@FindBy(xpath = "//a[text()='iPhone']")
+	private WebElement iphone;
+	@FindBy(xpath = "//a[text()='Apple']")
+	private WebElement apple;
+	@FindBy(xpath = "//span[text()='My Account']")
+	private WebElement Myacc;
+	@FindBy(xpath = "(//a[text()='Login'])[1]")
+	private WebElement loginopt;
+	@FindBy(xpath = "//input[@id=\"input-email\"]")
+	private WebElement Email;
+	@FindBy(xpath = "//input[@id=\"input-password\"]")
+	private WebElement pass;
+	@FindBy(xpath = "//input[@class=\"btn btn-primary\"]")
+	private WebElement loginbtn;
+	@FindBy(xpath = "//h2[text()='My Account']")
+	private WebElement myaccpage;
+
 	public Homepage(WebDriver driver) {
-		this.driver= driver;
-		PageFactory.initElements(driver,this);
-		
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 	}
-	
+
 	public String VerifyPageTitle() {
-		
-		String Title= driver.getTitle();
-		
+
+		String Title = driver.getTitle();
+
 		return Title;
 	}
-	
+
 	public boolean VerifyShoppingCartIcon() {
-		
-		boolean Actaulresult= shoppingcart.isDisplayed();
-		
+
+		WebElement shoppingcarticon = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Shopping Cart']")));
+		boolean Actaulresult = shoppingcarticon.isDisplayed();
+
 		return Actaulresult;
 	}
-	
+
 	public void ClickOnIphone() {
 		iphone.click();
 	}
-	
+
 	public String VerifyBrandNameOfiphone() {
-		
-		String Actualbrandname= apple.getText();
+
+		String Actualbrandname = apple.getText();
 		return Actualbrandname;
 	}
-	
+
 	public void ClickOnMyAccountDropdown() {
 		Myacc.click();
 	}
-	
+
 	public void clickOnLoginOption() {
 		loginopt.click();
 	}
-	
+
 	public void EnterEmailAddress(String email) {
 		Email.sendKeys(email);
-		
+
 	}
-	
+
 	public void EnterPassword(String Pass) {
 		pass.sendKeys(Pass);
 	}
-	
+
 	public void ClickedOnLoginButton() {
 		loginbtn.click();
 	}
-	
+
 	public String VerifyMyAccountPage() {
-		String ActualMyAcc= myaccpage.getText();
+		String ActualMyAcc = myaccpage.getText();
 		return ActualMyAcc;
 	}
 }
--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -3,17 +3,16 @@ package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(
 
-@CucumberOptions (
-		
-		features= {"E:\\Automation_Priyanka_VCTC\\Priyanka_VCTC\\QaFox_BDD_Automation\\src\\test\\resources\\App_Features"},
-		
-		glue = {"StepDefination", "Hooks"},
-		
-		plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty"}
-		
-	
-		)
+		features = {
+				"E:\\Automation_Priyanka_VCTC\\Priyanka_VCTC\\QaFox_BDD_Automation\\src\\test\\resources\\App_Features" },
+
+		glue = { "StepDefination", "Hooks" },
+
+		plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty" }
+
+)
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
 }
